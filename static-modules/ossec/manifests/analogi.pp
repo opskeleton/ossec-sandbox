e3c76f77b9cc42e1b6de837daf7e3d0b528a5513
Ronen Narkis <narkisr@gmail.com>
2013-10-30 01:00:29 +0200

    completed analogi

--- static-modules/ossec/manifests/analogi.pp
+++ static-modules/ossec/manifests/analogi.pp
@@ -1,13 +1,15 @@
 # setting up analogi
-class ossec::analogi {
+class ossec::analogi($dbuser='', $dbpass='', $dbhost=''){
 
-  package{['apache2', 'php5', 'libapache2-mod-php5', 'php5-mysql', 'git-core']:
+  package{['apache2', 'php5', 'php5-mysql']:
     ensure  => present
-  }
+  } ->
 
   git::clone {'analogi':
-    url   => 'git://github.com/ECSC/analogi.git',
-    dst   => '/var/www/analogi',
+    url     => 'git://github.com/ECSC/analogi.git',
+    dst     => '/var/www/analogi/',
+    owner   => root,
+    require => Package['apache2']
   } ->
 
   file { '/var/www/analogi/db_ossec.php':
@@ -15,5 +17,23 @@ class ossec::analogi {
     content => template('ossec/db_ossec.php.erb'),
     owner   => root,
     group   => root,
+    notify  => Service['apache2']
+  }
+
+  file { '/etc/apache2/sites-enabled/analogi':
+    ensure  => file,
+    mode    => '0644',
+    source  => 'puppet:///modules/ossec/analogi',
+    owner   => root,
+    group   => root,
+    notify  => Service['apache2'],
+    require => [Package['php5'] , Package['php5-mysql']]
+  }
+
+  service{'apache2':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => [Package['apache2'],]
   }
 }
