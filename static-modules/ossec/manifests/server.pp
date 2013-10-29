2ad911e9599214691de6fbe148f10f3f5b3cf5e0
Ronen Narkis <narkisr@gmail.com>
2013-10-30 01:00:47 +0200

    database setup works

--- static-modules/ossec/manifests/server.pp
+++ static-modules/ossec/manifests/server.pp
@@ -1,6 +1,12 @@
 # ossec server setup
 # see http://nolabnoparty.com/en/setup-ossec-with-mysql-and-analogi-in-centos-6/
-class ossec::server($dbpass='',$dbuser='',$mysql_root='') {
+class ossec::server($dbuser='', $dbpass='',$dbhost='') {
+
+  class{'ossec::analogi':
+    dbuser => $dbuser,
+    dbpass => $dbpass,
+    dbhost => $dbhost
+  }
 
   package{'ossec-hids-server':
     ensure   => present,
@@ -8,18 +14,9 @@ class ossec::server($dbpass='',$dbuser='',$mysql_root='') {
     source   => '/vagrant/ossec-hids-server_2.7.0-ubuntu12_amd64.deb'
   }
 
-  # package{['libmysql++-dev','build-essential']:
-  #   ensure  => present
-  # }
-
-  # package{"linux-headers-${::kernelrelease}":
-  #   ensure  => present
-  # }
-
   Exec['apt_update'] -> Package <||>
 
   class{'mysql::server':
-    # root_password => $mysql_root
   } ->
 
   mysql::db { 'ossec':
@@ -27,14 +24,14 @@ class ossec::server($dbpass='',$dbuser='',$mysql_root='') {
     password => $dbpass,
     host     => 'localhost',
     grant    => ['SELECT', 'UPDATE', 'INSERT', 'CREATE', 'DELETE', 'EXECUTE'],
-  } ~>
+  }
 
-  exec{'setyp-schema':
+  exec{'setup-schema':
     command     => 'mysql -u root -D ossec < /tmp/mysql.schema',
     user        => 'root',
-    path        => ['/usr/bin','/bin'],
-    refreshonly => true,
-    require     => File['/tmp/mysql.schema']
+    path        => '/usr/bin',
+    unless      => "/usr/bin/mysql -u root -D ossec -e 'select * from category'",
+    require     => [File['/tmp/mysql.schema'], Mysql::Db['ossec']]
   }
 
   file { '/tmp/mysql.schema':
@@ -43,6 +40,29 @@ class ossec::server($dbpass='',$dbuser='',$mysql_root='') {
     source=> 'puppet:///modules/ossec/mysql.schema',
     owner => root,
     group => root,
+  }
+
+  exec{'enable_database':
+    command     => '/var/ossec/bin/ossec-control enable database',
+    user        => 'root',
+    refreshonly => true,
+    require     => [Package['ossec-hids-server'], Exec['setup-shema']]
+  }
+
+  file { '/var/ossec/etc/ossec.conf':
+    ensure  => file,
+    mode    => '0440',
+    content => template('ossec/ossec.conf.erb'),
+    owner   => ossec,
+    group   => ossec,
+    require => Package['ossec-hids-server']
+  } ~>
+
+  service{'ossec-hids-server':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => Exec['setup-schema']
   }
 
 }
