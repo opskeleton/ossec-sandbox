f83f647410a81ecb4d721015d84f2581f6e13759
Ronen Narkis <narkisr@gmail.com>
2013-10-30 17:27:53 +0200

    adding agent conf and service

--- static-modules/ossec/manifests/agent.pp
+++ static-modules/ossec/manifests/agent.pp
@@ -1,7 +1,30 @@
 # ossec agent setup
-class ossec::agent {
+class ossec::agent($server_ip='') {
+  apt::source { 'ossec-ubuntu':
+    location    => 'http://ppa.launchpad.net/nicolas-zin/ossec-ubuntu/ubuntu',
+    release     => 'precise',
+    repos       => 'main',
+    key         => '0C4FF926',
+    key_server  => 'keyserver.ubuntu.com',
+  }
+
   package{'ossec-hids-agent':
     ensure  => present,
     require => Apt::Source['ossec-ubuntu']
+  }
+
+  file { '/var/ossec/etc/ossec.conf':
+    ensure  => file,
+    mode    => '0440',
+    content => template('ossec/agent-ossec.conf.erb'),
+    owner   => ossec,
+    group   => ossec,
+    require => Package['ossec-hids-agent']
+  } ~>
+
+  service{'ossec-hids-agent':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
   }
 }
