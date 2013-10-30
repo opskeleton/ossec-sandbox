3b257600e8a124a0e5025e78d66a1a784799c15a
Ronen Narkis <narkisr@gmail.com>
2013-10-30 17:28:22 +0200

    moving apt update to share init file

--- static-modules/ossec/manifests/init.pp
+++ static-modules/ossec/manifests/init.pp
@@ -1,16 +1,8 @@
 # ossec setup
 class ossec {
-
-  class { 'apt':
+  class{'apt':
     always_apt_update => true
   }
 
-  apt::source { 'ossec-ubuntu':
-    location          => 'http://ppa.launchpad.net/nicolas-zin/ossec-ubuntu/ubuntu',
-    release           => 'precise',
-    repos             => 'main',
-    key               => '0C4FF926',
-    key_server        => 'keyserver.ubuntu.com',
-  }
-
+  Exec['apt_update'] -> Package <||>
 }
